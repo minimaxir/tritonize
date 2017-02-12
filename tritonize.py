2b9b87ad20d16bf8130c077f037cec61dff94d6e
Max Woolf <max@minimaxir.com>
2017-02-12 09:35:40 -0800

    Scale blur programmically, only blur on load

--- tritonize.py
+++ tritonize.py
@@ -3,19 +3,21 @@ import numpy as np
 import math
 
 colors = [(26, 26, 26), (255, 255, 255), (44, 62, 80)]
-blur_factor = 400*400/200000
+blur_px_per_mp = 1
 
 
 def sigmoid(x):
 
     # result is between 0 and 1
     # since values between 0 and 255, (x-128)/32.0) converts range to about
-    # (-4, 4)
+    # (-4.0, 4.0)
 
     return 1 / (1 + math.exp(-((x - 128) / 32.0)))
 
 col = Image.open("test/profile.png")
-col = col.filter(ImageFilter.GaussianBlur(blur_factor))
+
+blur = (col.size[0] * col.size[1]) / (blur_px_per_mp * math.pow(10, 6))
+col = col.filter(ImageFilter.GaussianBlur(blur))
 
 gray = col.convert('L')
 col = col.convert('RGB')
@@ -28,5 +30,4 @@ for index, x in np.ndenumerate(bw):
     col[index] = colors[int(math.floor(threshold * len(colors)))]
 
 imfile = Image.fromarray(col)
-imfile = imfile.filter(ImageFilter.GaussianBlur(blur_factor))
 imfile.save("test/profile_color.png")
