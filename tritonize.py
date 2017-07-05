37b06d8e2ccde506fc7c203aed3f25f20e084988
Max Woolf <max@minimaxir.com>
2017-07-05 15:46:04 -0700

    RGBA support

--- tritonize.py
+++ tritonize.py
@@ -4,15 +4,24 @@ import os
 from scipy.misc import *
 from scipy.ndimage import *
 from itertools import permutations
+from ast import literal_eval as make_tuple
 
 
-def hex_to_rgb_triplet(triplet):
-    # http://stackoverflow.com/a/4296727
-    triplet = triplet.lstrip('#')
-    _NUMERALS = '0123456789abcdefABCDEF'
-    _HEXDEC = {v: int(v, 16)
-               for v in (x + y for x in _NUMERALS for y in _NUMERALS)}
-    return _HEXDEC[triplet[0:2]], _HEXDEC[triplet[2:4]], _HEXDEC[triplet[4:6]]
+def string_to_rgb_triplet(triplet):
+
+    if '#' in triplet:
+        # http://stackoverflow.com/a/4296727
+        triplet = triplet.lstrip('#')
+        _NUMERALS = '0123456789abcdefABCDEF'
+        _HEXDEC = {v: int(v, 16)
+                   for v in (x + y for x in _NUMERALS for y in _NUMERALS)}
+        return _HEXDEC[triplet[0:2]], _HEXDEC[triplet[2:4]],
+    _HEXDEC[triplet[4:6]], 255
+
+    else:
+        # https://stackoverflow.com/a/9763133
+        triplet = make_tuple(triplet)
+        return triplet + (255,) if len(triplet) == 3 else triplet
 
 
 def sigmoid(x):
@@ -34,7 +43,7 @@ def color_select(threshold_matrix, colors):
 
 
 def create_tritone(image_path, colors, blur):
-    colors_triplets = [hex_to_rgb_triplet(color) if isinstance(
+    colors_triplets = [string_to_rgb_triplet(color) if isinstance(
         color, str) else color for color in colors]
 
     color_list = list(permutations(colors_triplets))
@@ -56,7 +65,7 @@ def create_tritone(image_path, colors, blur):
     for i, color_set in enumerate(color_list):
         im_color = color_select(threshold_matrix, color_set)
 
-        imfile = toimage(im_color, mode='RGB')
+        imfile = toimage(im_color, mode='RGBA')
         imfile.save("tritonize/{}_{}.png".format(base_name, i + 1))
 
 if __name__ == '__main__':
@@ -67,7 +76,8 @@ if __name__ == '__main__':
                         required=True)
     parser.add_argument('-c',
                         '--colors', nargs='+',
-                        help='List of colors (as HEX strings)', required=True)
+                        help='List of colors (as HEX strings or '
+                        '3/4-element tuple strings)', required=True)
     parser.add_argument('-b',
                         '--blur', nargs='?', default=4.0, type=float,
                         help='Blur strength')
